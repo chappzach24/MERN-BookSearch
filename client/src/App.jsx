28eeecd9652686a262ac9490957d7ef32486d9f8
zachary chapman <zach.chapman24@gmail.com>
2024-04-18 18:40:26 -0400

    fixed graphql setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css';
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
